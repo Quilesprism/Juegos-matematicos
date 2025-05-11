f822b4791a1ab096027d13dbb0247290ca9a9fb5
Quilesprism <quilesxasterin8@gmail.com>
2025-05-10 21:01:00 -0500

    fixed: databases

---
+++ default.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juegos Matematicos</title>
+    <link rel="stylesheet" href="inicio/css/estilo_pag_principal.css">
+</head>
+<body>
+    <div class="contenedorg">
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_puzzlev1.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/puzzle" rel="nofollow"><img src="inicio/imagenes/tran_puzzlev1.png"></a>
+                </span>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_puzzlev2.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/puzzle_sin_color" rel="nofollow"><img src="inicio/imagenes/tran_puzzlev2.png"></a>
+                </span>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_escalerav2.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/escalera_v2" rel="nofollow"><img src="inicio/imagenes/tran_escalerav2.png"></a>
+                </span>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_escalerav1.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/escalera" rel="nofollow"><img src="inicio/imagenes/tran_escalerav1.png"></a>
+                </span>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_hanoi.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/hanoi" rel="nofollow"><img src="inicio/imagenes/tran_hanoi.png"></a>
+                </span>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_graficar.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/graficar" rel="nofollow"><img src="inicio/imagenes/tran_graficar.png"></a>
+                </span>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div class="cara cara1">
+                <span>
+                    <img src="inicio/imagenes/boton_reinas.png">
+                </span>
+            </div>
+            <div class="cara cara2">
+                <span>
+                    <a href="/reinas" rel="nofollow"><img src="inicio/imagenes/tran_reinas.png"></a>
+                </span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
